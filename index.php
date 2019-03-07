a143ff80314d198ec70339148540d514548a95a1
makin <makin@LAPTOP-2BNFCDEC>
2019-03-07 12:38:40 +0100

    Form1

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    <form action=".php" method="post">
+    <p>Voornaam:
+    <input type="text"  name="NR"/>*</p>
+    <p>
+    <input type="submit" value="Verzenden">
+    </p>
+    </form>
+    </body>
+</html>
